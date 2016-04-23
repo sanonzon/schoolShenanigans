5691f8c88fa7f324371cc8e333a41fa94cdfa1ba
suddan <pennsater@gmail.com>
2016-04-23 11:50:50 +0200

    Changed tree

--- calculator/Awesome/script.js
+++ calculator/Awesome/script.js
@@ -1,6 +1,58 @@
-$(function() {
-	var audio = new Audio('popcorn.mp3');
+/*
+audio.pause() på standard selection bara för sången är så vacker.
+Kan endast hantera tal -> operand -> tal -> =
 
+*/
+
+
+var audio = new Audio("popcorn.mp3");
+
+function changeTheme(v) {
+	switch (v) {
+		case 1:
+		$("#theme_selector").attr({
+			rel: "stylesheet",
+			type: "text/css",
+			href: "style1.css"
+		});
+		audio.play();
+		break;
+		case 2:
+		$("#theme_selector").attr({
+			rel: "stylesheet",
+			type: "text/css",
+			href: "style3.css"
+		});
+		break;
+		case 3:
+		$("#theme_selector").attr({
+			rel: "stylesheet",
+			type: "text/css",
+			href: "style2.css"
+		});
+
+		break;
+		case 4:
+		$("#theme_selector").attr({
+			rel: "stylesheet",
+			type: "text/css",
+			href: "style4.css"
+		});
+		break;
+		default:
+		$("#theme_selector").attr({
+			rel: "stylesheet",
+			type: "text/css",
+			href: "style.css"
+		});
+		audio.pause();
+		audio.currentTime = 0;
+		break;
+	}
+}
+
+
+$(function() {
 	var result = null;
 	var test = true;
 	var x = null;
@@ -8,66 +60,62 @@ $(function() {
 	var operand = null;
 	var error = "";
 
-	$("button").on("click", function() {
-		audio.play();
-	});
-
 	$("button.number").on("click", function() {
 		$("#display").text($("#display").text() + $(this).text());
 	});
 
 	$("button.dot").on("click", function() {
-		$("#display").text($("#display").text() + $(this).text());
+			$("#display").text($("#display").text() + $(this).text());
 	});
 
 	$("button.mult").on("click", function() {
-		x = $("#display").text();
+			x = $("#display").text();
 		$("#display").text("");
 		operand = $(this).text();
 	});
 
 	$("button.divide").on("click", function() {
-		x = $("#display").text();
+			x = $("#display").text();
 		$("#display").text("");
 		operand = $(this).text();
 	});
 
 	$("button.plus").on("click", function() {
-		x = $("#display").text();
+			x = $("#display").text();
+		lastClickWasOpenrand = true;
 		$("#display").text("");
 		operand = $(this).text();
 	});
 
 	$("button.minus").on("click", function() {
-		x = $("#display").text();
+			x = $("#display").text();
+		lastClickWasOpenrand = true;
 		$("#display").text("");
 		operand = $(this).text();
 	});
 
 	$("button.reset").on("click", function() {
-		$("#display").text("");
 		result = null;
-		 test = true;
-		 x = null;
-		 y = null;
-		 operand = null;
-		 error = "";
-	/*	$("#errorHandler").text("");
+		test = true;
+		x = null;
+		y = null;
+		operand = null;
+		error = "";
+
+		$("#display").text("");
 		$("#errorX").text("");
 		$("#errorY").text("");
-		$("#withEval").text("");
-		*/
+
 	});
 
 	$("button.equals").on("click", function() {
-
 		if ($("#display").text().length > 0) {
 			y = $("#display").text();
 		}
 		console.log("x = " + x);
 		console.log("y = " + y);
 		if ((x == null || x.length == 0) || (y == null || y.length == 0)) {
-			error = "null";
+			error = "ERROR";
 			test = false;
 		}
 
@@ -79,16 +127,13 @@ $(function() {
 		console.log("pre-number check: " + test);
 		if (test) {
 			if (!(/^[0-9]*$/.test(x) || /^[0-9]*\.[0-9]*$/.test(x))) {
-				error = "Första talet är dåligt.";
+				error = "ERROR";
 				test = false;
 			}
 			if (!(/^[0-9]*$/.test(y) || /^[0-9]*\.[0-9]*$/.test(y))) {
 				test = false;
-				if (error.length > 0) {
-					error = "Båda talen är dåliga.";
-				}
-				else {
-					error = "Andra talet är dåligt.";
+				if (error.length == 0) {
+					error = "ERROR";
 				}
 			}
 		}
@@ -136,24 +181,21 @@ $(function() {
 			result = result.toFixed(decimalCount);
 
 			$("#display").text(result);
-			/*		evalString = "" + x + operand + y;
-			$("#withEval").text("With eval: " + eval(evalString));
-			*/
+
+			var loggen = "" + x.toString() + " " + operand.toString() + " " + y.toString() + " = " + result.toString();
+			console.log(loggen);
 		}
 
 		else {
-			console.log("Error posted");
 
-			$("#errorHandler").text(error);
 			if (x != null && y != null) {
-			$("#errorX").text("Tal 1: " + x);
-			$("#errorY").text("Tal 2: " + y);
+				$("#errorX").text("Tal 1: " + x);
+				$("#errorY").text("Tal 2: " + y);
 			}
-			
-		$("#display").text(error);
 
-	}
+			$("#display").text(error);
 
-});
+		}
 
+	});
 });
